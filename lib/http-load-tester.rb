2d153142c891dde35eccf7637fd4a0164f4ba452
Tom Clarke <tom.clarke@u2i.com>
2010-01-24 17:38:33 -0500

    Removing some unused requires

--- lib/http-load-tester.rb
+++ lib/http-load-tester.rb
@@ -1,9 +1,7 @@
 require 'httpclient'
-require 'blockenspiel'
 
 require 'http-load-tester/callbacks'
 require 'http-load-tester/dsl'
 require 'http-load-tester/scenario'
-require 'http-load-tester/scenario_client'
 require 'http-load-tester/scenario_factory'
 require 'http-load-tester/tester'
