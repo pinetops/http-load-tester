9cf8e2d7ee66f871f0fbcffd368c4685435dadfc
Tom Clarke <tom.clarke@u2i.com>
2010-01-24 21:10:39 -0500

    Making the Tester thread safe

--- lib/http-load-tester/tester.rb
+++ lib/http-load-tester/tester.rb
@@ -1,8 +1,12 @@
+require 'thread'
+
 module HttpLoadTester
   PROCS = 10
   REQUESTS = 500
 
   class Tester
+    include MonitorMixin
+
     attr_reader :request_limit
     
     class << self
@@ -20,6 +24,7 @@ module HttpLoadTester
       @requests = 0
       @count = 0
       @request_limit = REQUESTS
+      @mutex = Mutex.new
     end
 
     def scenarios
@@ -81,23 +86,25 @@ module HttpLoadTester
     end
 
     def increment
-      if @requests == PROCS
-        @start_time = Time.new
-        puts
-        puts "Starting"
-      end
-
-      if @requests == PROCS + @request_limit
-        @stop_time = Time.new
-        puts
-        puts "Stopping"
-      end
+      @mutex.synchronize do
+        if @requests == PROCS
+          @start_time = Time.new
+          puts
+          puts "Starting"
+        end
+        
+        if @requests == PROCS + @request_limit
+          @stop_time = Time.new
+          puts
+          puts "Stopping"
+        end
       
-      if @requests >= PROCS && @count < @request_limit
-        @count += 1
+        if @requests >= PROCS && @count < @request_limit
+          @count += 1
+        end
+        
+        @requests += 1
       end
-
-      @requests += 1
     end
   end
 end
